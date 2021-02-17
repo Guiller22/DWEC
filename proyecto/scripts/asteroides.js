ea7905b7bed4c88d92148b3e4112af72ac8339c0
guille <miito2010v2@hotmail.com>
2021-02-17 14:28:28 +0100

    cambios

---
+++ proyecto/scripts/asteroides.js
@@ -0,0 +1,128 @@
+function codigo() {
+	var peticion, bd, transaccion, almacenPlanetas, almacenAsteroides;
+
+	
+	function refrescarListas() {
+		if (window.indexedDB) {
+			peticion = window.indexedDB.open("Planetas");
+
+			peticion.onsuccess = function (evento) {
+				bd = evento.target.result;
+							
+				transaccion = bd.transaction(bd.objectStoreNames, "readwrite");
+				almacen = transaccion.objectStore("Asteroides");
+
+				document.getElementById("listaAsteroides").innerHTML = "";
+
+				var peticionCursor = almacen.openCursor();
+				peticionCursor.onsuccess = function (evento) {
+					var cursor = peticionCursor.result;
+
+					if(cursor) {
+						insertarElementoControlLista(cursor.value);
+
+						cursor.continue(); //continue incrementa el cursor una posición
+					} else {
+						console.log("FIN");
+					}
+				}
+				document.getElementById("id").value = "";
+				document.getElementById("nombre").value = "";
+				document.getElementById("dimensiones").value = "";
+				document.getElementById("anyo").value = "";
+
+				//bd.close();
+			};
+
+
+			peticion.onerror = function (evento) {
+				alert("No se ha creado la base de datos: " + evento.target.errorCode);
+			};
+
+
+			peticion.onupgradeneeded = function (evento) {
+				console.log("Upgradeneeded");
+			};
+
+		} else {
+			console.log("IndexedDB no está soportado");
+		}
+
+		document.getElementById("botonCargar").disabled = true;
+	}
+
+
+	function insertarElementoControlLista(_asteroide) {
+		//var htmlTexto = libro.isbn + "  <----->  " + libro.titulo;
+		var htmlTexto = _asteroide.nombre;
+		var listaItem = document.createElement("option");
+		listaItem.setAttribute("value",_asteroide.id);
+		listaItem.textContent = htmlTexto;
+
+		var lista = document.getElementById("listaAsteroides");
+		lista.appendChild(listaItem);
+
+		listaItem.onclick = function () {
+			var id = lista.options[lista.selectedIndex].value;
+			var transaccionEvento = bd.transaction(bd.objectStoreNames, "readwrite");
+			var almacenEvento = transaccionEvento.objectStore("Asteroides");
+			var registroEvento = almacenEvento.get((id));
+
+			registroEvento.onsuccess = function(evento) {
+				document.getElementById("id").value = registroEvento.result.id;
+				document.getElementById("nombre").value = registroEvento.result.nombre;
+				document.getElementById("dimensiones").value = registroEvento.result.dimensiones;
+				document.getElementById("anyo").value = registroEvento.result.anyo_desc;
+			}
+		}
+	}
+
+
+	/*****INSERTAR ********************************************/
+	document.getElementById("botonInsertar").onclick = function () {
+		var transaccionInsertar = bd.transaction(bd.objectStoreNames,"readwrite");
+		var almacenInsertar = transaccionInsertar.objectStore("Asteroides");
+		var nuevoAsteroide= {};
+
+		nuevoAsteroide.id = document.getElementById("id").value;
+		nuevoAsteroide.nombre = document.getElementById("nombre").value;
+		nuevoAsteroide.dimensiones = document.getElementById("dimensiones").value;
+		nuevoAsteroide.anyo_desc = document.getElementById("anyo").value;
+		
+
+		almacenInsertar.add(nuevoAsteroide);
+		refrescarListas();
+	}
+
+
+	document.getElementById("botonModificar").onclick = function () {
+		var transaccionModificar = bd.transaction(bd.objectStoreNames,"readwrite");
+		var almacenModificar = transaccionModificar.objectStore("Asteroides");
+		var nuevoAsteroide = {};
+
+		nuevoAsteroide.id = document.getElementById("id").value;
+		nuevoAsteroide.nombre = document.getElementById("nombre").value;
+		nuevoAsteroide.dimensiones = document.getElementById("dimensiones").value;
+		nuevoAsteroide.anyo_desc = document.getElementById("anyo").value;
+		almacenModificar.put(nuevoAsteroide);
+		refrescarListas();
+	}
+
+	/***************************************************************
+	 * ELIMINAR ****************************************************
+	 **************************************************************/
+	document.getElementById("botonEliminar").onclick = function () {
+		var transaccionEliminar = bd.transaction(bd.objectStoreNames,"readwrite");
+		var almacenEliminar = transaccionEliminar.objectStore("Asteroides");
+
+		peticionEliminacion = almacenEliminar.delete(document.getElementById("id").value);
+		peticionEliminacion.onsuccess = function () {
+			alert("Asteroide eliminado")
+		}
+		refrescarListas();
+	}
+
+	document.getElementById("botonCargar").onclick = refrescarListas;
+}
+
+window.onload = codigo;
---
+++ proyecto/scripts/planetas.js
@@ -0,0 +1,127 @@
+function codigo() {
+	var peticion, bd, transaccion, almacen;
+
+	
+	function refrescarLista() {
+		if (window.indexedDB) {
+			peticion = window.indexedDB.open("Planetas");
+
+			peticion.onsuccess = function (evento) {
+				bd = evento.target.result;
+							
+				transaccion = bd.transaction(bd.objectStoreNames, "readwrite");
+				almacen = transaccion.objectStore("Planetas");
+
+				document.getElementById("listaPlanetas").innerHTML = "";
+
+				var peticionCursor = almacen.openCursor();
+				peticionCursor.onsuccess = function (evento) {
+					var cursor = peticionCursor.result;
+
+					if(cursor) {
+						insertarElementoControlLista(cursor.value);
+
+						cursor.continue(); //continue incrementa el cursor una posición
+					} else {
+						console.log("FIN");
+					}
+				}
+				document.getElementById("id").value = "";
+				document.getElementById("nombre").value = "";
+				document.getElementById("desc").value = "";
+				document.getElementById("anyo").value = "";
+				//bd.close();
+			};
+
+
+			peticion.onerror = function (evento) {
+				alert("No se ha creado la base de datos: " + event.target.errorCode);
+			};
+
+
+			peticion.onupgradeneeded = function (evento) {
+				console.log("Upgradeneeded");
+			};
+
+		} else {
+			console.log("IndexedDB no está soportado");
+		}
+
+		document.getElementById("botonCargar").disabled = true;
+	}
+
+
+	function insertarElementoControlLista(_planeta) {
+		//var htmlTexto = libro.isbn + "  <----->  " + libro.titulo;
+		var htmlTexto = _planeta.nombre;
+		var listaItem = document.createElement("option");
+		listaItem.setAttribute("value",_planeta.id);
+		listaItem.textContent = htmlTexto;
+
+		var lista = document.getElementById("listaPlanetas");
+		lista.appendChild(listaItem);
+
+		listaItem.onclick = function () {
+			var id = lista.options[lista.selectedIndex].value;
+			var transaccionEvento = bd.transaction(bd.objectStoreNames, "readwrite");
+			var almacenEvento = transaccionEvento.objectStore("Planetas");
+			var registroEvento = almacenEvento.get(id);
+
+			registroEvento.onsuccess = function(evento) {
+				document.getElementById("id").value = registroEvento.result.id;
+				document.getElementById("nombre").value = registroEvento.result.nombre;
+				document.getElementById("anyo").value = registroEvento.result.anyo;
+				document.getElementById("desc").value = registroEvento.result.desc;
+			}
+		}
+	
+	}
+
+	
+	document.getElementById("botonInsertar").onclick = function () {
+		var transaccionInsertar = bd.transaction(bd.objectStoreNames,"readwrite");
+		var almacenInsertar = transaccionInsertar.objectStore("Planetas");
+		var nuevoPlaneta = {};
+
+		nuevoPlaneta.id = document.getElementById("id").value;
+		nuevoPlaneta.anyo = document.getElementById("anyo").value;
+		nuevoPlaneta.nombre = document.getElementById("nombre").value;
+		nuevoPlaneta.desc = document.getElementById("desc").value;
+		
+
+		almacenInsertar.add(nuevoPlaneta);
+		refrescarLista();
+	}
+
+
+	document.getElementById("botonModificar").onclick = function () {
+		var transaccionModificar = bd.transaction(bd.objectStoreNames,"readwrite");
+		var almacenModificar = transaccionModificar.objectStore("Planetas");
+		var nuevoPlaneta = {};
+
+		nuevoPlaneta.id = document.getElementById("id").value;
+		nuevoPlaneta.anyo = document.getElementById("anyo").value;
+		nuevoPlaneta.nombre = document.getElementById("nombre").value;
+		nuevoPlaneta.desc = document.getElementById("desc").value;
+		almacenModificar.put(nuevoPlaneta);
+		refrescarLista();
+	}
+
+	/***************************************************************
+	 * ELIMINAR ****************************************************
+	 **************************************************************/
+	document.getElementById("botonEliminar").onclick = function () {
+		var transaccionEliminar = bd.transaction(bd.objectStoreNames,"readwrite");
+		var almacenEliminar = transaccionEliminar.objectStore("Planetas");
+
+		peticionEliminacion = almacenEliminar.delete(document.getElementById("id").value);
+		peticionEliminacion.onsuccess = function () {
+			alert("Planeta eliminado")
+		}
+		refrescarLista();
+	}
+
+	document.getElementById("botonCargar").onclick = refrescarLista;
+}
+
+window.onload = codigo;
