9a1845d5c9d5eafb4d57e34b89ea0a10e97068be
guille <miito2010v2@hotmail.com>
2021-04-22 16:18:41 +0200

    as

--- Trimestre3/Ejemplo_08/js/funciones.js
+++ Trimestre3/Ejemplo_08/js/funciones.js
@@ -1,5 +1,22 @@
 document.getElementById("procesar").addEventListener("click",function () {
- 
-   
-});
+    var elementos = document.getElementById("caja1").getElementsByTagName("*");
+    var etiquetas = [];
 
+    Array.from(elementos,x => {
+        if(etiquetas.indexOf(x.tagName)==-1) {
+            if(x.parentElement === document.getElementById("caja1")) {
+                etiquetas.push(x.tagName);
+            }
+        }
+    });
+
+    etiquetas.forEach(x => {
+        var serieElementos = document.getElementById("caja1").getElementsByTagName(x);
+
+        Array.from(serieElementos,y => {
+            document.getElementById("caja2").appendChild(y.cloneNode(true));
+        });
+    });
+
+    document.getElementById("caja1").innerHTML = "";
+});
